8fe8f44320d1a9741fc897805189dec284764024
Usuario <marprogr@protonmail.com>
2025-09-08 22:09:44 -0300

    added operations enum

--- maquina.h
+++ maquina.h
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #define MEM 16384
 #define ENT 8
 #define REG 32
@@ -11,7 +12,11 @@ typedef enum {
     LAR = 0,MAR,MBR,IP,OPC,OP1,OP2,
     EAX = 10,EBX,ECX,EDX,EEX,EFX,AC,CC,
     CS = 26,DS } registro;
-
+typedef enum {
+    MOV = 10,ADD,SUB,MUL,DIV,CMP,SHL,SHR,SAR,AND,OR,XOR,SWAP,LDL,LDH,RND,
+    SYS = 0, JMP, JZ, JP, JN, JNZ, JNP, JNN, NOT,
+    STOP = 0x0F
+} operacion;
 typedef struct {
     unsigned short base;
     unsigned short tamanio; } infoSegmento;              //unsigned short tiene exact. 2 bytes
