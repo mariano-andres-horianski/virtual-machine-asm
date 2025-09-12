4031fdfe9e881e2ccb3e5ecc371c50ebac8b9c39
Usuario <marprogr@protonmail.com>
2025-09-12 20:27:33 -0300

    updated this branch with changes in other branches, moved function pointer to main  and fixed some naming on the functions

--- maquina.c
+++ maquina.c
@@ -1,4 +1,6 @@
 #include "maquina.h"
+#include <stdlib.h>
+#include <stdio.h>
 void inicioRegistro(uint32_t reg[]){
     reg[CS] = 0x00000000;
     reg[DS] = 0x00010000;
@@ -13,7 +15,7 @@ void inicioTablaSegmento(infoSegmento tabla[],uint16_t tamanioCod){
 }
 void leerEncabezado(char nombre[],uint32_t registros[REG],infoSegmento tablaSegmento[ENT],uint8_t memoria[MEM]){
     FILE *arch;
-    char ident[5];    
+    char ident[6];    
     char version;
     uint16_t tamanio;
     int resultado = 0;
@@ -23,7 +25,8 @@ void leerEncabezado(char nombre[],uint32_t registros[REG],infoSegmento tablaSegm
     if(arch!=NULL){
         fseek(arch,0,0); // inicio de archivo
         if( fread(ident,sizeof(char),5,arch) == 5){
-            if (strcmp(ident,"VMX25")){ 
+            ident[5] = '\0';
+            if (strcmp(ident,"VMX25") == 0){ 
                 fread(&version,sizeof(char),1,arch);
                 if(version == 1){
                     fread(&tamanio,sizeof(uint16_t),1,arch);
@@ -41,7 +44,7 @@ void leerEncabezado(char nombre[],uint32_t registros[REG],infoSegmento tablaSegm
         inicioTablaSegmento(tablaSegmento,tamanio);
     }
     else 
-        printf("ERROR: no se pudo leer el resultado");
+        printf("ERROR: no se pudo leer el resultado\n");
 
 }
 uint32_t calcDirFisica(infoSegmento tablaSegmento[ENT],uint32_t regIP,int cantBytes){
@@ -55,16 +58,17 @@ uint32_t calcDirFisica(infoSegmento tablaSegmento[ENT],uint32_t regIP,int cantBy
     if(tablaSegmento[numSegmento].base <= direcFisica && limSegmento >= limAcceso)
         return direcFisica;
     else{
-        printf("segmentation fault\n"); 
+        printf("SEGMENTATION  FAULT\n"); // detecta uno de los 3 errores que se deben tener en cuenta segun requisitos
         return 0xFFFFFFFF;
     }
 }
 
-void leerInstrucciones(uint8_t instruccion, uint8_t memoria[], uint32_t registros[REG], uint32_t *dirFisica){
+void leerInstrucciones(uint8_t instruccion, uint8_t memoria[], uint32_t registros[REG]){
     //usar los OP para calcular los bytes que necesitamos leer y poner en lectura
     
     uint32_t lectura;
     uint8_t cantByteA,cantByteB;
+    uint32_t posicion;
     
     registros[OPC]=instruccion & 0x1F;
     registros[OP2]=instruccion >> 6;
@@ -75,16 +79,18 @@ void leerInstrucciones(uint8_t instruccion, uint8_t memoria[], uint32_t registro
     if (registros[OP1]==0 && registros[OP2]!=0)
         registros[OP1]=registros[OP2];
     if ((OPC!=0x0F && registros[OP1]==0)||(OPC<=0x1F && OPC>=0x10 && registros[OP2]==0)){ //reviso si es una operacion invalida
-        printf("Operacion invalida");
-        *dirFisica = 0xFFFFFFFF;
+        printf("ERROR: operacion invalida\n");
+        memoria[MAR] = 0xFFFFFFFF;
     }
     else {
         //aca hay que avanzar el IP en 1 y leer byte a byte OP2 veces
+        posicion = memoria[registros[IP] + 1];
+
+        operandos(&lectura,registros[OP2],registros,memoria,&posicion);  /// agregar a .h ----------------------------------------?
         registros[OP2]=registros[OP2] << 24;
         registros[OP2]=registros[OP2] | lectura;
-        
-        
         //aca hay que avanzar el IP en 1 y leer byte a byte OP1 veces
+        operandos(&lectura,registros[OP1],registros,memoria,&posicion);
         registros[OP1]=registros[OP1] << 24;
         registros[OP1]=registros[OP1] | lectura;
 
@@ -96,13 +102,71 @@ void leerInstrucciones(uint8_t instruccion, uint8_t memoria[], uint32_t registro
 
 void ejecucion(uint32_t registros[REG],infoSegmento tablaSegmento[ENT],uint8_t memoria[MEM]){
     uint8_t instruccion;
-    uint32_t dirFisica;
+    uint8_t codInstruccion;
+    uint8_t Tipo1,Tipo2;
+    uint32_t operando2,operando1;
     
     registros[IP] = registros[CS];
-    dirFisica = calcDirFisica(tablaSegmento,registros[IP],1);  // 1?? ref de la cantidad de bytes de acceso
-    while (dirFisica != 0xFFFFFFFF){
-        leerInstrucciones(memoria[registros[IP]], memoria, registros, &dirFisica); //la dirFisica es una posicion del vector memoria que tenemos que chequear constantemente
+    memoria[MAR] = calcDirFisica(tablaSegmento,registros[IP],1);  // 1?? ref de la cantidad de bytes de acceso
+    while (memoria[MAR] != 0xFFFFFFFF){
+        leerInstrucciones(memoria[registros[IP]], memoria, registros); //la dirFisica es una posicion del vector memoria que tenemos que chequear constantemente
         //aca iria una funcion que ejecute las instrucciones en base a lo que hay en el vector registros en las posiciones OP1, OP2 y OPC
         //lo haria accediendo a posiciones de un vector de punteros a funciones donde cada una es una instruccion
+        
+        codInstruccion = memoria[registros[IP]];
+        Tipo1 = (registros[OP1] >> 30 ) & 0x3;
+        Tipo2 = (registros[OP2] >> 30 ) & 0x3;
+        operando1 = registros[OP1] & 0x00FFFFFF;
+        operando2 = registros[OP2] & 0x00FFFFFF;
+       // instrucciones[codInstruccion](Tipo1,Tipo2,OP1,OP2,registros,memoria);
     }
+}
+uint32_t get(uint8_t tipo_operando, uint32_t operando,uint32_t registros[], uint8_t memoria[]){
+    if (tipo_operando == 0x01)
+        return registros[operando];
+    else if (tipo_operando == 0x02)
+        return operando;
+    else return memoria[operando];
+}
+void operandos(uint32_t *lectura,uint32_t tipo,uint32_t registros[],uint8_t memoria[],uint32_t *posicion){
+    switch (tipo){
+    case 0b01: {
+        *lectura = memoria[*posicion]; 
+        *posicion += 1;
+        break;
+    }
+    case 0b10: {
+        *lectura = (memoria[*posicion] << 8)  |  memoria[*posicion + 1];
+        *posicion += 2;
+    }
+    case 0b11: {
+        *lectura = (memoria[*posicion] << 16) | (memoria[*posicion + 1] << 8) | (memoria[*posicion + 2]);
+        *posicion += 3;
+    }
+    }
+}
+void ResultadoOperacion(uint8_t Tipo1,uint32_t registros[],uint8_t memoria[],int resultado,infoSegmento tablaSegmentos[]){
+    uint32_t codRegistro;
+    if(Tipo1 == 0b01){ // registro
+        codRegistro = registros[OP1] & 0x00FFFFFF;
+        registros[codRegistro] = resultado;
+    }
+    else {
+        if(Tipo1 == 0b11) { // memoria
+            memoria[MAR] = calcDirFisica(tablaSegmentos,registros[IP],1);
+            memoria[memoria[MAR]] =  (resultado >> 24) & 0x000000FF;
+            memoria[memoria[MAR] + 1] = (resultado >> 16) & 0x00FF;
+            memoria[memoria[MAR] + 2] = (resultado >> 8) & 0x0000FF;
+            memoria[memoria[MAR] + 3] = resultado & 0x000000FF;
+        }
+    }
+}
+void actualizarCC(uint32_t registros[],uint32_t resultado){
+    if(resultado < 0)
+        registros[CC] = 0x80000000;
+    else
+        if(resultado == 0)
+            registros[CC] = 0x40000000;
+        else 
+            registros[CC] = 0x0;
 }
--- maquina.h
+++ maquina.h
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #define MEM 16384
 #define ENT 8
 #define REG 32
@@ -11,27 +10,45 @@
 typedef enum {
     LAR = 0,MAR,MBR,IP,OPC,OP1,OP2,
     EAX = 10,EBX,ECX,EDX,EEX,EFX,AC,CC,
-    CS = 26,DS } registro;
-typedef enum {
-    MOV = 0x10,ADD,SUB,MUL,DIV,CMP,SHL,SHR,SAR,AND,OR,XOR,SWAP,LDL,LDH,RND,
-    SYS = 0, JMP, JZ, JP, JN, JNZ, JNP, JNN, NOT,
-    STOP = 0x0F
-} operacion;
+    CS = 26,DS } registros;
+
 typedef struct {
     unsigned short base;
     unsigned short tamanio; } infoSegmento;              //unsigned short tiene exact. 2 bytes
+
 
 void inicioRegistro(uint32_t reg[]);
 void inicioTablaSegmento(infoSegmento tabla[],uint16_t tamanio);
 void leerEncabezado(char nombre[],uint32_t registros[REG],infoSegmento tablaSegmento[ENT],uint8_t memoria[MEM]);
 uint32_t calcDirFisica(infoSegmento tablaSegmento[ENT],uint32_t regIP,int cantBytes);
-int operando(uint8_t tipo);
 void ejecucion(uint32_t registros[REG],infoSegmento tablaSegmento[ENT],uint8_t memoria[MEM]);
-//un operando
-void JMP(uint32_t OP1, uint32_t registros[]);
-void JZ(uint32_t OP1, uint32_t registros[]);
-void JP(uint32_t OP1, uint32_t registros[]);
-void JN(uint32_t OP1, uint32_t registros[]);
-void JNZ(uint32_t OP1, uint32_t registros[]);
-void JNP(uint32_t OP1, uint32_t registros[]);
-void JNN(uint32_t OP1, uint32_t registros[]);
+uint32_t get(uint8_t tipo_operando, uint32_t operando,uint32_t registros[], uint8_t memoria[]);
+void JMP(uint32_t registros[]);
+void JZ(uint32_t registros[]);
+void JP(uint32_t registros[]);
+void JN(uint32_t registros[]);
+void JNZ(uint32_t registros[]);
+void JNP(uint32_t registros[]);
+void JNN(uint32_t registros[]);
+
+void leerInstrucciones(uint8_t instruccion, uint8_t memoria[], uint32_t registros[REG]);
+void operandos(uint32_t *lectura,uint32_t tipo,uint32_t registros[],uint8_t memoria[],uint32_t *posicion);
+void ResultadoOperacion(uint8_t Tipo1,uint32_t registros[],uint8_t memoria[], int resultado,infoSegmento tablaSegmentos[]);
+void actualizarCC(uint32_t registros[],uint32_t resultado);
+void ADD(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void SUB(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void MUL(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void DIV(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void CMP(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void SHL(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void SHR(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void SAR(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void AND(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void OR(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void XOR(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void SWAP(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void LDL(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void LDH(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void NOT(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void STOP(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
+void NO_ACCESIBLE(uint8_t Tipo1,uint8_t Tipo2,uint32_t registros[],uint8_t memoria[],infoSegmento tablaSegmentos[]);
