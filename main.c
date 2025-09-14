0deddbf580c66a9f67fe9fa3abb5188a3eb9d1e4
Jazmin <jazzy.p.toi@gmail.com>
2025-09-13 22:37:09 -0300

    agrego el disassembler y los parametros en instrucciones

--- main.c
+++ main.c
@@ -5,7 +5,11 @@ int main()
     uint8_t memoria[MEM];                         // memoria de 16 kib
     infoSegmento tablaSegmento[ENT];                    // tabla de segmentos sin inicializar
     uint32_t registros[REG];                            // cada pos tiene disponible 4 bytes >> con unsigned char tiene 1 byte
-    void (*instrucciones[31])(void) = {SYS,JMP,JZ,JP,JN,JNZ,JNP,JNN,NOT,NO_ACCESIBLE,NO_ACCESIBLE,NO_ACCESIBLE,NO_ACCESIBLE,NO_ACCESIBLE,NO_ACCESIBLE,STOP,MOV,ADD,SUB,MUL,DIV,CMP,SHL,SHR,SAR,AND,OR,XOR,SWAP,LDL,LDH,RND};
+   void (*instrucciones[31])(uint8_t, uint8_t, uint32_t[], uint8_t[], infoSegmento[]) = { //faltaba declarar parametros
+    SYS, JMP, JZ, JP, JN, JNZ, JNP, JNN, NOT,
+    NO_ACCESIBLE, NO_ACCESIBLE, NO_ACCESIBLE, NO_ACCESIBLE, NO_ACCESIBLE, NO_ACCESIBLE,
+    STOP, MOV, ADD, SUB, MUL, DIV, CMP, SHL, SHR, SAR, AND, OR, XOR, SWAP, LDL, LDH, RND
+};
     leerEncabezado("sample.vmx",registros,tablaSegmento,memoria);
     printf("inicio de ejecucion");
     ejecucion(registros,tablaSegmento,memoria);
